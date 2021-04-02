21af106c00c51f67e82edce23699dda10323dbfc
DINNERman <shrwnwilliams@gmail.com>
2021-04-02 11:10:51 -0400

    added placeholder for pageload so there's no weird styling when the cards haven't loaded in

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,3 @@
-var cardDisplay = $("card-display");
-var imageOne = $("#image-one");
-var imageTwo = $("#image-two");
-var imageThree = $("#image-three");
-
 function init() {
     getApiInfo();
 }
@@ -13,33 +8,15 @@ function getApiInfo() {
     fetch(requestUrl).then(function (response){
         return response.json();
     }).then(function (data){
-        // generateRandomCards();
-        console.log(data.data[0].images.small)
         var ranNum1 = Math.floor(Math.random() * data.data.length)
         var ranNum2 = Math.floor(Math.random() * data.data.length)
         var ranNum3 = Math.floor(Math.random() * data.data.length)
-        console.log(ranNum1);
-        console.log(ranNum2);
-        console.log(ranNum3);
-        var imageEl1 = $("<img>");
-        var imageEl2 = $("<img>");
-        var imageEl3 = $("<img>");
+        var imageEl1 = $("#image-one");
+        var imageEl2 = $("#image-two");
+        var imageEl3 = $("#image-three");
         imageEl1.attr("src", data.data[ranNum1].images.small);
-        imageOne.append(imageEl1);
         imageEl2.attr("src", data.data[ranNum2].images.small);
-        imageTwo.append(imageEl2);
         imageEl3.attr("src", data.data[ranNum3].images.small);
-        imageThree.append(imageEl3);
-
-        // generateRandomCards();
-        // function generateRandomCards(cardIndex) {
-        //     var number = Math.floor(Math.random() * cardIndex);
-        //     for (var i = 0; i < 3; i++) {
-        //         console.log(data.data[number].images.small)
-        //         var imgUrl = data.data[number].images.small;
-        //         var imageEl = $("<img>");
-        //         imageEl.attr("src", imgUrl);
-        //         card.append(priceEl);
             })
         };
 
